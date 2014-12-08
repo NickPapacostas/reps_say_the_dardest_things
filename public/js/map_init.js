573e9dd812dfec888e5d25b8e20f9d89e1e83d7f
Nicky P <np1249a@gmail.com>
2014-12-08 14:16:03 +0200

    lists displaying! working on getting inputs to work

--- public/js/map_init.js
+++ public/js/map_init.js
@@ -5,13 +5,56 @@ var mapInit = function (datamap) {
 }
 
 var stateClick = function (state) {
-  console.log(state.id)
+  getPhrases(state.id)
 }
+
+var getPhrases = function(state) {
+  var Sunlight = {
+    endpoint: 'http://capitolwords.org/api/phrases.json',
+    defaultParams: {
+      apikey: "3e45fa3f533947169823f6097289a378",
+      entity_type: 'state',
+      entity_value: state, 
+      n: 3
+    },
+
+  };
+  var queryParams = {
+    data: Sunlight.defaultParams,
+    type: "GET",
+    dataType: 'jsonp',
+    phrase: "fiscal",
+  }
+  var response = jQuery.ajax(
+    Sunlight.endpoint, 
+    queryParams
+    ).done(addResponse)
+  
+  function addResponse(response){
+    var responseObj = {};
+    responseObj[state] = response;
+    window.response = responseObj;
+    map.updateChoropleth(responseObj)
+
+  }
+}
+
 
 var map = new Datamap({
   element: document.getElementById('container'),
   scope: 'usa',
   done: function(datamap) {
     mapInit(datamap);
+  },
+  data: {}, 
+  geographyConfig: {
+    popupTemplate: function(geo, data) {
+      if (data) {
+        var phraseList = data.slice(0,5).map(function(r){ return {phrase: r.ngram}});
+        var template = Handlebars.compile($('#top-phrase-list').html());
+        return template({state: geo.id, phrases: phraseList});        
+      }
+    }
   }
 });
+window.map = map;
